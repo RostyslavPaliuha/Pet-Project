30f828cab4f930c47e8192819f01abcec8900f4f
Rostyslav Paliuha <rostyslavpaliuha@gmail.com>
2018-01-07 17:51:01 +0200

    friendrepo it test additionals

--- src/test/java/ua/com/social/demo/repository/impl/FriendListRepositoryTest.java
+++ src/test/java/ua/com/social/demo/repository/impl/FriendListRepositoryTest.java
@@ -2,6 +2,7 @@ package ua.com.social.demo.repository.impl;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.jdbc.Sql;
@@ -18,29 +19,17 @@ import static org.junit.Assert.*;
         @Sql(executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD, scripts = "classpath:sql/cleardata.sql")})
 
 public class FriendListRepositoryTest {
+    @Autowired
+    private AccountRepository accountRepository;
+    @Autowired
+    private ProfileRepository profileRepository;
+    @Autowired
+    private ProfileDetailsRepository detailsRepository;
     @Test
-    public void persist() throws Exception {
+    public void addFriend_getFriends_deleteFriend() throws Exception {
 
     }
 
-    @Test
-    public void delete() throws Exception {
-    }
 
-    @Test
-    public void get() throws Exception {
-    }
-
-    @Test
-    public void persistAndRetrieveId() throws Exception {
-    }
-
-    @Test
-    public void getAll() throws Exception {
-    }
-
-    @Test
-    public void getFriends() throws Exception {
-    }
 
 }
